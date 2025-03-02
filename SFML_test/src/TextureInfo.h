0dd67f59611e4daf922d782c61f5160dd273b2e5
icyyoung719 <1073182632@qq.com>
2025-03-02 13:24:33 +0800

    add more elements to TextureInfo

--- SFML_test/src/TextureInfo.h
+++ SFML_test/src/TextureInfo.h
@@ -9,7 +9,17 @@ struct TextureInfo
     // the time between each frame
     float switchTime;
     // where the real model starts
-    sf::Vector2i position;
+    sf::Vector2i startPixel;
     // the real model size
-    sf::Vector2i size;
+    sf::Vector2i modelSize;
+    // the png size
+    sf::Vector2i pngSize;
+
+    TextureInfo(sf::Texture* texture, sf::Vector2u imageCount, float switchTime, sf::Vector2i startPixel, sf::Vector2i modelSize)
+        : texture(texture), imageCount(imageCount), switchTime(switchTime), startPixel(startPixel), modelSize(modelSize)
+    {
+        pngSize = sf::Vector2i(texture->getSize().x / imageCount.x, texture->getSize().y / imageCount.y);
+    }
+
+    TextureInfo() : texture(nullptr), imageCount(sf::Vector2u(0, 0)), switchTime(0.0f), startPixel(sf::Vector2i(0, 0)), modelSize(sf::Vector2i(0, 0)), pngSize(sf::Vector2i(0, 0)) {}
 };
